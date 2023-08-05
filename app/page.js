e659434357b6545a51e6558af5f2adc3da51fe5d
redfit <daisuke.fuchiwaki@kaminashi.com>
2023-08-06 02:10:17 +0900

    Add Pagination with the Meta Object Returned from Strapi in Next.js 13

--- app/page.js
+++ app/page.js
@@ -3,9 +3,12 @@ import Main from "@/components/Main";
 import Title from "@/components/Title";
 import Description from "@/components/Description";
 import List from "@/components/List";
+import Pagination from "@/components/Pagination";
 
-export default async function Home() {
-  const data = await getPosts();
+export default async function Home({ searchParams }) {
+  const { page } = searchParams;
+  const pageParam = page === undefined ? 1 : page;
+  const data = await getPosts(pageParam);
 
   return (
     <PageContainer>
@@ -13,14 +16,15 @@ export default async function Home() {
         <Title>words from others (for me)</Title>
         <Description>a recipe for disaster</Description>
         <List allPosts={data} />
+        <Pagination paginationData={data.meta.pagination} />
       </Main>
     </PageContainer>
   );
 }
 
-async function getPosts() {
+async function getPosts(pageParam) {
   const res = await fetch(`
-  ${process.env.API_URL}/api/posts`);
+  ${process.env.API_URL}/api/posts?pagination[page]=${pageParam}&pagination[pageSize]=2`);
 
   if (!res.ok) {
     throw new Error("Failed to fetch data from API");
