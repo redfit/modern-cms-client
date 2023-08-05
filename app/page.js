2e7e8c5c075732a3289690f1390c216a0f16d93c
redfit <daisuke.fuchiwaki@kaminashi.com>
2023-08-05 21:13:12 +0900

    Create and Edit a List Component to Match Design Guideline in Next.js 13

--- app/page.js
+++ app/page.js
@@ -2,31 +2,21 @@ import PageContainer from "@/components/PageContainer";
 import Main from "@/components/Main";
 import Title from "@/components/Title";
 import Description from "@/components/Description";
+import List from "@/components/List";
 
 export default async function Home() {
-
   const data = await getPosts();
 
   return (
     <PageContainer>
       <Main>
-        <Title>
-          words from others (for me)
-        </Title>
-        <Description>
-          a recipe for disaster
-        </Description>
-
-        {data.data.map((post) => (
-          <div key={post.attributes.slug}>
-            {post.attributes.title}
-          </div>
-        ))}
+        <Title>words from others (for me)</Title>
+        <Description>a recipe for disaster</Description>
+        <List allPosts={data} />
       </Main>
     </PageContainer>
   );
 }
-
 
 async function getPosts() {
   const res = await fetch(`
